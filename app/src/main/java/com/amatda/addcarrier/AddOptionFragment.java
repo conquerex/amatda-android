3eef043aac5c0b8203ca9b2e8da4a63a8d36a82a
손종국 <conquerex@gmail.com>
2019-02-16 17:28:56 +0900

    Options list

--- app/src/main/java/com/amatda/addcarrier/AddOptionFragment.java
+++ app/src/main/java/com/amatda/addcarrier/AddOptionFragment.java
@@ -12,19 +12,55 @@ import com.amatda.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AddOptionFragment extends Fragment {
+public class AddOptionFragment extends Fragment implements View.OnClickListener {
 
+    private View layoutAddOptionNone;
+    private View layoutAddOptionEssential;
+    private View layoutAddOptionSwimming;
+    private View layoutAddOptionWinter;
+    private View layoutAddOptionCamping;
+    private View layoutAddOptionBusiness;
+    private View layoutAddOptionBaby;
 
     public AddOptionFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_option, container, false);
+        View view = inflater.inflate(R.layout.fragment_add_option, container, false);
+
+        layoutAddOptionNone = view.findViewById(R.id.layoutAddOptionNone);
+        layoutAddOptionEssential = view.findViewById(R.id.layoutAddOptionEssential);
+        layoutAddOptionSwimming = view.findViewById(R.id.layoutAddOptionSwimming);
+        layoutAddOptionWinter = view.findViewById(R.id.layoutAddOptionWinter);
+        layoutAddOptionCamping = view.findViewById(R.id.layoutAddOptionCamping);
+        layoutAddOptionBusiness = view.findViewById(R.id.layoutAddOptionBusiness);
+        layoutAddOptionBaby = view.findViewById(R.id.layoutAddOptionBaby);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.layoutAddOptionNone:
+                break;
+            case R.id.layoutAddOptionEssential:
+                break;
+            case R.id.layoutAddOptionSwimming:
+                break;
+            case R.id.layoutAddOptionWinter:
+                break;
+            case R.id.layoutAddOptionCamping:
+                break;
+            case R.id.layoutAddOptionBusiness:
+                break;
+            case R.id.layoutAddOptionBaby:
+                break;
+            default:
+                break;
+        }
+    }
 }
