9a00f0e43d4f42763c212f98882f8402f616d4c0
손종국 <conquerex@gmail.com>
2019-02-17 04:47:10 +0900

    Add splash activity, Lottie lib

---
+++ app/src/main/java/com/amatda/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.amatda.splash;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.amatda.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
