e49a986657e41776ed562b863299b8a3db4e35c5
손종국 <conquerex@gmail.com>
2019-02-17 05:25:24 +0900

    Lottie sample

--- app/src/main/java/com/amatda/splash/SplashActivity.java
+++ app/src/main/java/com/amatda/splash/SplashActivity.java
@@ -3,13 +3,19 @@ package com.amatda.splash;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.amatda.R;
 
 public class SplashActivity extends Activity {
 
+    LottieAnimationView lottieSplash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        lottieSplash = findViewById(R.id.lottieSplash);
+        lottieSplash.setAnimation("ani_09_passport.json");
+        lottieSplash.playAnimation();
     }
 }
